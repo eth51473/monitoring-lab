08db851e01ec8ef084b0f80bf87c28fc0fc31e8e
Ethan Bird <eth51473@gmail.com>
2021-09-14 12:07:48 -0600

    changed a dumb thing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const Rollbar = require("rollbar")
+// const Rollbar = require("rollbar")
 
 const app = express();
 app.use(express.json())
@@ -8,7 +8,6 @@ app.use(express.json())
 
 app.get("/",(req,res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"))
-  rollbar.info("HTML file served successfully!")
 })
 
 
