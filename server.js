ce04ad6b8cac5786dbe7b87fc70ae6e002eb2df4
Ethan Bird <eth51473@gmail.com>
2021-09-14 11:58:05 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const Rollbar = require("rollbar")
+
+const app = express();
+app.use(express.json())
+
+
+app.get("/",(req,res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+  rollbar.info("HTML file served successfully!")
+})
+
+
+const port = process.env.PORT || 4545;
+app.listen(port, () =>{
+  console.log(`server is up on ${port}!`)
+})
