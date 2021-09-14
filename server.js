e808b0f7e4b92fb64972eb021fd7e1c65def599f
Ethan Bird <eth51473@gmail.com>
2021-09-14 12:28:38 -0600

    fixed endpoint name

--- server.js
+++ server.js
@@ -15,14 +15,13 @@ let rollbar = new Rollbar({
 
 
 // record a generic message and send it to Rollbar
-rollbar.log("Hello world!");
 
 
 app.get("/",(req,res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
-app.get("/erro",(req,res) => {
+app.post("/error",(req,res) => {
   try {
     nonExistentFunction();
   } catch (error) {
