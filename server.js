dab8f05089236039bf711c80cae2365ca3a984d7
Ethan Bird <eth51473@gmail.com>
2021-09-14 12:22:46 -0600

    added endpoint with a function that doesn't work

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const path = require("path");
-// const Rollbar = require("rollbar")
+const Rollbar = require("rollbar")
 
 const app = express();
 app.use(express.json())
+
+
+let rollbar = new Rollbar({
+  accessToken: "fa6291d24ead4fe5a4039e5c5f12e4f9",
+  captureUncaught: true,
+  captureUnhandledRejections: true
+  
+})
 
 
 app.get("/",(req,res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
+app.get("/erro",(req,res) => {
+  try {
+    nonExistentFunction();
+  } catch (error) {
+    rollbar.error('that didnt work ')
+    console.error(error);
+    
+  }})
 
+app.use(rollbar.errorHandler())
 const port = process.env.PORT || 4545;
 app.listen(port, () =>{
   console.log(`server is up on ${port}!`)
